d87252b98363992a4e46e000ed4c6e7f7479b9cf
Jan Vollmer <jan@vllmr.dev>
2026-02-06 21:14:59 +0100

    fix: mirror requires-python
    Signed-off-by: Jan Vollmer <jan@vllmr.dev>

--- mirror.py
+++ mirror.py
@@ -14,40 +14,58 @@ from pathlib import Path
 
 import urllib3
 from packaging.requirements import Requirement
+from packaging.specifiers import SpecifierSet
 from packaging.version import Version
 
 PACKAGE = "zizmor"
+
+
+class Release(typing.NamedTuple):
+    version: Version
+    requires_python: SpecifierSet
 
 
 def main():
     with open(Path(__file__).parent / "pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
 
-    all_versions = get_all_versions()
+    all_releases = get_all_versions()
     current_version = get_current_version(pyproject=pyproject)
-    target_versions = [v for v in all_versions if v > current_version]
+    target_releases = [r for r in all_releases if r.version > current_version]
 
-    for version in target_versions:
-        paths = process_version(version)
+    for release in target_releases:
+        paths = process_release(release)
         if subprocess.check_output(["git", "status", "-s"]).strip():
             subprocess.run(["git", "add", *paths], check=True)
-            subprocess.run(["git", "commit", "-m", f"Mirror: {version}"], check=True)
-            subprocess.run(["git", "tag", f"v{version}"], check=True)
+            subprocess.run(
+                ["git", "commit", "-m", f"Mirror: {release.version}"], check=True
+            )
+            subprocess.run(["git", "tag", f"v{release.version}"], check=True)
         else:
-            print(f"No change v{version}")
+            print(f"No change v{release}")
 
 
-def get_all_versions() -> list[Version]:
+def get_all_versions() -> list[Release]:
     response = urllib3.request("GET", f"https://pypi.org/pypi/{PACKAGE}/json")
     if response.status != 200:
-        raise RuntimeError("Failed to fetch versions from pypi")
+        raise RuntimeError("Failed to fetch releases from pypi")
 
-    versions = [
-        Version(release)
-        for release in response.json()["releases"]
-        if not Version(release).is_prerelease
-    ]
-    return sorted(versions)
+    releases: list[Release] = []
+    for v, release_files in response.json()["releases"].items():
+        version = Version(v)
+
+        if version.is_prerelease:
+            continue
+
+        requires_python = SpecifierSet(">=3.10")
+        for f in release_files:
+            if f["python_version"] == "py3" and (spec := f["requires_python"]):
+                requires_python = SpecifierSet(spec)
+                break
+
+        releases.append(Release(version=version, requires_python=requires_python))
+
+    return sorted(releases, key=lambda r: r.version)
 
 
 def get_current_version(pyproject: dict) -> Version:
@@ -65,14 +83,23 @@ def get_current_version(pyproject: dict) -> Version:
     return Version(specifiers[0].version)
 
 
-def process_version(version: Version) -> typing.Sequence[str]:
+def process_release(release: Release) -> typing.Sequence[str]:
     def replace_pyproject_toml(content: str) -> str:
-        return re.sub(rf'"{PACKAGE}==.*"', f'"{PACKAGE}=={version}"', content)
+        replaced_version = re.sub(
+            rf'"{PACKAGE}==.*"', f'"{PACKAGE}=={release.version}"', content
+        )
+        return re.sub(
+            r'requires-python = ".*"',
+            f'requires-python = "{release.requires_python}"',
+            replaced_version,
+        )
 
     def replace_readme_md(content: str) -> str:
-        content = re.sub(r"rev: v\d+\.\d+\.\d+", f"rev: v{version}", content)
+        content = re.sub(r"rev: v\d+\.\d+\.\d+", f"rev: v{release.version}", content)
         return re.sub(
-            rf"/{PACKAGE}/\d+\.\d+\.\d+\.svg", f"/{PACKAGE}/{version}.svg", content
+            rf"/{PACKAGE}/\d+\.\d+\.\d+\.svg",
+            f"/{PACKAGE}/{release.version}.svg",
+            content,
         )
 
     paths = {
